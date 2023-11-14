d3f95cae111a6996eb6e23b84af8a58c8fbc45be
Ori Komemie <orikomemi90@gmail.com>
2023-11-14 12:43:46 +0200

    some refactor tables

--- tables/hospitalsVacancy.js
+++ tables/hospitalsVacancy.js
@@ -3,36 +3,36 @@ import { Table } from './table.js'
 const hospitalsVacancyData = [
   {
     hospitalName: 'בלינסון', 
-    generalVacancyPercentage: 8,
-    internalVacancyPercentage: 10,
+    generalVacancyPercentage: 71,
+    internalVacancyPercentage: 74,
     id: 'hospital-0',
   },
 
   {
     hospitalName: 'מאיר', 
-    generalVacancyPercentage: 2,
-    internalVacancyPercentage: 11,
+    generalVacancyPercentage: 70,
+    internalVacancyPercentage: 85,
     id: 'hospital-1',
   },
 
   {
     hospitalName: 'שיבא', 
-    generalVacancyPercentage: 5,
-    internalVacancyPercentage: 7,
+    generalVacancyPercentage: 52,
+    internalVacancyPercentage: 20,
     id: 'hospital-2',
   },
 
   {
     hospitalName: 'רמב"ם', 
-    generalVacancyPercentage: 8,
-    internalVacancyPercentage: 3,
+    generalVacancyPercentage: 84,
+    internalVacancyPercentage: 32,
     id: 'hospital-3',
   },
 
   {
     hospitalName: 'אסף הרופא', 
-    generalVacancyPercentage: 15,
-    internalVacancyPercentage: 20,
+    generalVacancyPercentage: 3,
+    internalVacancyPercentage: 11,
     id: 'hospital-4',
   },
   
@@ -55,9 +55,10 @@ class HospitalVacancyTable extends Table{
 
            //if key has precentage on it - need to add precentages bars
           if(key.includes("Percentage")){
+            const completionBarColor = record[key] < 30 ? '#8eb0c0' :  (record[key] < 75 ? '#69c3f5' : '#de7483')
             htmlRecordMarkup += `<div class="data-item data-item-with-precentage-bar">
                                     <div class="precentage-container">
-                                      <div class="precentage-bar" style="width: ${record[key]}%;"></div>
+                                      <div class="precentage-bar" style="width: ${record[key]}%; background-color: ${completionBarColor};"></div>
                                     </div>
                                     
                                     ${record[key]}%
